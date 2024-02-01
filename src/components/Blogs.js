711a30c60ebb4c107379d9da6085a9a52a484b1f
Chimpdu <dyxlut@126.com>
2024-02-01 15:08:34 +0200

    conditional rendering for delete button

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,24 +1,40 @@
-import React from 'react'
-function Blogs({blogs, deleteBlog}) {
-    
-    
+import React, { useState } from 'react';
+
+function Blogs({ blogs, deleteBlog }) {
+    const [hoveredBlogId, setHoveredBlogId] = useState(null);
+
+    const onMouseOver = (id) => {
+        setHoveredBlogId(id);
+    };
+
+    const onMouseLeave = () => {
+        setHoveredBlogId(null);
+    };
+
     return (
-    <div className='blogs'>
-        {
-            blogs.map((blog) => {return (
-                <div className="blog-card" key={blog.id}>
-                <h2>{blog.title}</h2>
-                <p>{blog.body}</p>
-                <p>{blog.author}</p>
-                <button className='waves-effect waves-light btn blog-del-btn' onClick={()=>{deleteBlog(blog.id)}}>Delete</button>
+        <div className='blogs'>
+            {blogs.map((blog) => (
+                <div 
+                    className="blog-card" 
+                    key={blog.id} 
+                    onMouseOver={() => onMouseOver(blog.id)} 
+                    onMouseLeave={onMouseLeave}
+                >
+                    <h2>{blog.title}</h2>
+                    <p>{blog.body}</p>
+                    <p>{blog.author}</p>
+                    {hoveredBlogId === blog.id && (
+                        <button 
+                            className='waves-effect waves-light btn blog-del-btn' 
+                            onClick={() => deleteBlog(blog.id)}
+                        >
+                            Delete
+                        </button>
+                    )}
                 </div>
-      )})
-    
-        
-    }
-      
-    </div>
-  )
+            ))}
+        </div>
+    );
 }
 
-export default Blogs
+export default Blogs;

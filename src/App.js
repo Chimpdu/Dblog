3e8fa3809d6e442ffe47ef93f70cc4e4dc10b765
Chimpdu <dyxlut@126.com>
2024-02-01 13:33:40 +0200

    adding React router

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import './App.css';
 import './components/Nav'
 import Nav from './components/Nav';
 import Home from './components/Home';
+import About from './components/About';
+import AddBlog from './components/AddBlog';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
 function App() {
   return (
-    <div className="App">
-      <Nav/>
-      <Home/>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav/>
+        <Routes>
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='/about' element={<About/>}></Route>
+          <Route path ='/add' element={<AddBlog/>}></Route>
+        </Routes>
+       
+      </div>
+    </Router>
   );
 }
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,17 +1,31 @@
-import React from 'react'
-
+import {useEffect} from 'react'
+import {Link} from 'react-router-dom'
+import M from 'materialize-css';
+//npm i materialize-css@next is needed as we need to initialize the sidenav.
 function Nav() {
+  useEffect(()=>{
+    // Initialize the sidenav
+    M.Sidenav.init(document.querySelectorAll('.sidenav'));
+  }, [])
   return (
-    <nav className='navbar'>
-      <div className="nav-wrapper">
-        <a href="/" className="brand-logo">Dlog</a>
-        <ul id="nav-mobile" className="right hide-on-med-and-down">
-          <li><a href="/">Home</a></li>
-          <li><a href="/#">mock</a></li>
-          <li><a href="/#">mock</a></li>
-        </ul>
-      </div>
-</nav>
+    <>
+      <nav className="nav-bar">
+        <div className="nav-wrapper">
+          <Link to="/" className="brand-logo">Dlog</Link>
+          <Link to="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></Link>
+          <ul className="right hide-on-med-and-down">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/add">Add blog</Link></li>
+            <li><Link to="/about">About</Link></li>
+          </ul>
+        </div>
+      </nav>
+      <ul className="sidenav" id="mobile-demo">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/add">Add blog</Link></li>
+        <li><Link to="/about">About</Link></li>
+      </ul>
+    </>
   )
 }
 
